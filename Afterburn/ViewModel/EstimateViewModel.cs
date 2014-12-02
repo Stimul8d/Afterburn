dd398385eb8a305d31b7e4d1e7faa171028760b4
Stimul8d <Stimul8d@hotmail.com>
2014-12-02 22:29:24 +0000

    Made scaling changes awesome

--- Afterburn/ViewModel/EstimateViewModel.cs
+++ Afterburn/ViewModel/EstimateViewModel.cs
@@ -8,10 +8,22 @@ namespace Afterburn.ViewModel
     public class EstimateViewModel : ViewModelBase
     {
         public ObservableCollection<TaskViewModel> Tasks { get; set; }
+        public TaskViewModel Distractions { get; set; }
 
         public EstimateViewModel()
         {
             CreateTasks();
+            Distractions = new TaskViewModel();
+            for (int i = 0; i < 50; i++)
+            {
+                var date = DateTime.Now.AddDays(i);
+                var update = new TaskUpdateViewModel
+                {
+                    Date = date,
+                    Hours = 15 - i + 0.25
+                };
+                Distractions.Updates.Add(update);
+            }
         }
 
         private void CreateTasks()
@@ -25,7 +37,7 @@ namespace Afterburn.ViewModel
                 Hours = 6.25
             };
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 50; i++)
             {
                 var date = DateTime.Now.AddDays(i);
                 var update = new TaskUpdateViewModel
