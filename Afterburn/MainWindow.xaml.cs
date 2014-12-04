0b451ddb37d3ab3ab2ac9fbf6a0ae3559be21a45
Stimul8d <Stimul8d@hotmail.com>
2014-12-04 22:10:36 +0000

    Added Settings Flyout

--- Afterburn/MainWindow.xaml.cs
+++ Afterburn/MainWindow.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using System.Windows;
 using Afterburn.ViewModel;
+using MahApps.Metro.Controls;
 
 namespace Afterburn
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         /// <summary>
         /// Initializes a new instance of the MainWindow class.
--- Afterburn/ViewModel/MainViewModel.cs
+++ Afterburn/ViewModel/MainViewModel.cs
@@ -7,6 +7,8 @@ namespace Afterburn.ViewModel
 
     public class MainViewModel : ViewModelBase
     {
+        const int num = 10;
+
         public ObservableCollection<TaskViewModel> Tasks { get; set; }
         public TaskViewModel Distractions { get; set; }
         public TaskViewModel TasksRollup { get; set; }
@@ -17,13 +19,13 @@ namespace Afterburn.ViewModel
             CreateDistractions();
             
             TasksRollup = new TaskViewModel();
-            for (int i = 0; i < 15; i++)
+            for (int i = 0; i < num; i++)
             {
                 var date = DateTime.Now.AddDays(i);
                 var update = new TaskUpdateViewModel
                 {
                     Date = date,
-                    Hours = 15 - i + 0.25
+                    Hours = num - i + 0.25
                 };
                 update.Hours *= 13;
                 TasksRollup.Updates.Add(update);
@@ -33,13 +35,13 @@ namespace Afterburn.ViewModel
         private void CreateDistractions()
         {
             Distractions = new TaskViewModel();
-            for (int i = 0; i < 15; i++)
+            for (int i = 0; i < num; i++)
             {
                 var date = DateTime.Now.AddDays(i);
                 var update = new TaskUpdateViewModel
                 {
                     Date = date,
-                    Hours = 15 - i + 0.25
+                    Hours = num - i + 0.25
                 };
                 Distractions.Updates.Add(update);
             }
@@ -56,13 +58,13 @@ namespace Afterburn.ViewModel
                 Hours = 6.25
             };
 
-            for (int i = 0; i < 15; i++)
+            for (int i = 0; i < num; i++)
             {
                 var date = DateTime.Now.AddDays(i);
                 var update = new TaskUpdateViewModel
                 {
                     Date = date,
-                    Hours = 15 - i + 0.25
+                    Hours = num - i + 0.25
                 };
                 t.Updates.Add(update);
             }
@@ -70,6 +72,36 @@ namespace Afterburn.ViewModel
             for (int i = 0; i < 13; i++)
             {
                 Tasks.Add(t);
+            }
+        }
+
+        /// <summary>
+        /// The <see cref="HoursPerDay" /> property's name.
+        /// </summary>
+        public const string HoursPerDayPropertyName = "HoursPerDay";
+
+        private double hoursPerDay = 8.5;
+
+        /// <summary>
+        /// Sets and gets the HoursPerDay property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public double HoursPerDay
+        {
+            get
+            {
+                return hoursPerDay;
+            }
+
+            set
+            {
+                if (hoursPerDay == value)
+                {
+                    return;
+                }
+
+                hoursPerDay = value;
+                RaisePropertyChanged(HoursPerDayPropertyName);
             }
         }
     }
